38d4cf6d3c864abfc500b2fbc4dccb4dea28336b
MLM MOVIE STORE <86915159+arjunsangu@users.noreply.github.com>
2021-07-07 08:15:48 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -38,7 +38,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    '🎭 ⭕️ FOR YOUR QUERY, CONTACT ME ⭕️', url="https://t.me/UFSChatBot"
+                                    '⭕🎭 MAIN CHANEL 🎭⭕️', url="https://t.me/mlm_movie_2"
                                 )
                         ]
                     ]
@@ -57,7 +57,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    '🎭 ⭕️ FOR YOUR QUERY, CONTACT ME ⭕️', url="https://t.me/UFSChatBot"
+                                    '⭕🎭 MAIN CHANEL 🎭⭕️', url="https://t.me/mlm_movie_2"
                                 )
                         ]
                     ]
@@ -76,7 +76,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    '🎭 ⭕️ FOR YOUR QUERY, CONTACT ME ⭕️', url="https://t.me/UFSChatBot"
+                                    '⭕🎭 MAIN CHANEL 🎭⭕️', url="https://t.me/mlm_movie_2"
                                 )
                         ]
                     ]
@@ -89,10 +89,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/joinchat/TRlZZilyh-MVa66t'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://t.me/joinchat/YS-WlsUC9nFiOWM0')
+        InlineKeyboardButton('⚜️MAIN GROUP⚜️', url='https://t.me/MLM_MOVIE_STORE'),
+        InlineKeyboardButton('⚜️ MAIN CHANEL⚜️', url ='https://t.me/mlm_movie_2')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/joinchat/YS-WlsUC9nFiOWM0')
+        InlineKeyboardButton('⚜️NEW MOVIE UPDATEDS⚜️', url='https://t.me/mlm_movies_update')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
